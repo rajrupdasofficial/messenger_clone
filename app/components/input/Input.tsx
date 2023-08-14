c402732b33054aadc74f8d04a3b1536a90f78c53
RajrupDasid <rajrupofficialid@protonmail.com>
2023-08-14 21:51:33 +0530

    Changes Include
    Input for login hasbeen redefined and configured
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- app/components/input/Input.tsx
+++ app/components/input/Input.tsx
@@ -11,7 +11,6 @@ import {
     label: string;
     id: string;
     type: string;
-    autoComplete:string;
     required: boolean;
     register: UseFormRegister<FieldValues>;
     errors: FieldErros;
@@ -48,6 +47,27 @@ const Input: React.FC<InputProps> = ({
                 autoComplete={id}
                 disabled={disabled}
                 {...register(id, { required })}
+                className={clsx(`
+                    form-input
+                    block
+                    w-full
+                    border-0
+                    py-1.5
+                    text-gray-900
+                    shadow-sm
+                    ring-1
+                    ring-inset
+                    ring-gray-300
+                    placeholder:text-gary-400
+                    focus:ring-2
+                    focus:ring-inset
+                    focus:ring-sky-600
+                    sm:text-sm
+                    sm:leading-6
+                `,
+                errors[id] && 'focus:ring-rose-500',
+                disabled && 'opacity-50 cursor-default'
+                 )}
                 />
 
             </div>
