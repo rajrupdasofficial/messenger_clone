4948eb8ee52d4c42497bcda2bb486a9afbc243e6
RajrupDasid <rajrupofficialid@protonmail.com>
2023-08-16 20:00:49 +0530

    Critical build error fixed
    removed the export value from authOptions because it already been exported when defining the api params
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -7,7 +7,7 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "@/app/lib/prismadb";
 import Email from "next-auth/providers/email";
 
-export const authOptions: AuthOptions = {
+const authOptions: AuthOptions = {
     adapter: PrismaAdapter(prisma),
     providers:[
         GithubProvider({
