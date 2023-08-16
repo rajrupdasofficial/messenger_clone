9e0c989dab83823e32298e667c2570eaa4751e0d
RajrupDasid <rajrupofficialid@protonmail.com>
2023-08-17 00:38:47 +0530

    Critical build error fixed
    build error is going on due to error import now hasbeen sorted
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,12 @@
 import bcrypt from "bcrypt";
-import NextAuth, { AuthOptions } from "next-auth/next";
+import NextAuth from "next-auth/next";
 import  CredentialsProvider  from "next-auth/providers/credentials";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "@/app/lib/prismadb";
 import Email from "next-auth/providers/email";
+import { AuthOptions } from "next-auth";
 
 const authOptions: AuthOptions = {
     adapter: PrismaAdapter(prisma),
